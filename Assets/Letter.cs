ad192032b79717a5dcba8dd47c69b4f550ef2ccd
Aram Joseph <ajoseph@utc4me.org>
2017-03-21 10:26:21 -0400

    boi, we got letters

---
+++ Assets/Letter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter : MonoBehaviour {
+    [SerializeField]
+    [Range(0,1)]
+    float startingOffset;
+
+
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.Play("LetterFloaty", -1, startingOffset);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
