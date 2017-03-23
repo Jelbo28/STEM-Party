cc6a51782a42abf75eaa089dae4a700c8a600976
Aram Joseph <ajoseph@utc4me.org>
2017-03-22 23:00:22 -0400

    Col

---
+++ Assets/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    Transform anchor;
+    [SerializeField]
+    float dampTime;
+    [SerializeField]
+    float moveSpeed;
+    [SerializeField]
+    float rotateSpeed;
+    private Camera cam;
+    private Vector3 velocity = Vector3.zero;
+    // Use this for initialization
+    void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = moveSpeed * Time.deltaTime;
+        cam.transform.position = Vector3.MoveTowards(transform.position, anchor.position, step);
+        float turn = rotateSpeed * Time.deltaTime;
+        cam.transform.rotation = Quaternion.RotateTowards(transform.rotation,Quaternion.LookRotation(target.transform.position - transform.position), turn);
+    }
+}
---
+++ Assets/DistanceCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceCalculator : MonoBehaviour {
+
+    [SerializeField]
+    Transform startPoint;
+    private float dist;
+
+	// Use this for initialization
+	void Start () {
+        while(transform.position.y <= 100 || transform.position.y >= 300)
+        {
+            transform.position = Random.onUnitSphere * 3845;
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        dist = Vector3.Distance(transform.position, startPoint.position);
+        if (dist == 3845)
+        {
+            Debug.Log("Goochi");
+        }
+	}
+}
