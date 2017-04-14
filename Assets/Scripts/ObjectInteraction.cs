b5381a5b422a42dbf2ce00c1425f76cbca088076
Aram Joseph <ajoseph@utc4me.org>
2017-04-14 10:29:24 -0400

    Woo

---
+++ Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour {
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+            Debug.DrawLine(ray.origin, hit.point);
+    }
+}
