2eb9577cc45c9fca2cd5d5fde44f2afbddd2cd6e
Aram Joseph <ajoseph@utc4me.org>
2017-04-24 18:16:21 -0400

    Finally, smooth ui

--- Assets/Scripts/PageSlider.cs
+++ Assets/Scripts/PageSlider.cs
@@ -2,57 +2,119 @@
 
 public class PageSlider : MonoBehaviour
 {
-    [SerializeField] private bool left;
-    [SerializeField] private float[] origX;
-    [SerializeField] private Transform[] pages;
-    [SerializeField] private float speed;
-    [SerializeField] private bool transition;
-    // Use this for initialization
-    private void Start()
+    //   [SerializeField]
+    //   private float speed;
+    //   [SerializeField]
+    //   private bool transition;
+    //   [SerializeField] private bool left;
+    //   [SerializeField] private float xMove;
+    //   [SerializeField]
+    //private  float[] toX = new float[5];
+    //   [SerializeField]
+    //   private  float[] fromX = new float[5];
+
+    //   [SerializeField] private Transform[] pages;
+
+    //   // Use this for initialization
+    //   private void Start()
+    //   {
+    //       SetPosition();
+
+
+    //   }
+
+    //   // Update is called once per frame
+    //   private void Update()
+    //   {
+    //       if (transition)
+    //       {
+    //           for (int i = 0; i < pages.Length; i++)
+    //           {
+    //               if (left && pages[i].GetComponent<RectTransform>().localPosition.x >= fromX[i] - toX[i])
+    //               {
+    //                   pages[i].GetComponent<RectTransform>().Translate(-speed * Time.deltaTime, 0, 0);
+    //               }
+    //               else if (!left && pages[i].GetComponent<RectTransform>().localPosition.x <= fromX[i] + toX[i])
+    //               {
+    //                   pages[i].GetComponent<RectTransform>().Translate(speed * Time.deltaTime, 0, 0);
+    //               }
+    //               else
+    //               {
+
+
+    //                   SetPosition();
+    //               }
+    //           }
+
+    //       }
+
+    //   }
+
+    //   void SetPosition()
+    //   {
+    //       for (int i = 0; i < pages.Length; i++)
+    //       {
+    //           Vector3 bob = new Vector3(fromX[i] - toX[i], pages[i].GetComponent<RectTransform>().localPosition.y, pages[i].GetComponent<RectTransform>().localPosition.z);
+    //           pages[i].GetComponent<RectTransform>().position = bob;
+    //           toX[i] = xMove;
+    //           fromX[i] = pages[i].GetComponent<RectTransform>().localPosition.x;
+    //       }
+    //       transition = false;
+    //   }
+
+    private RectTransform thisRect;
+    [SerializeField]
+    Vector3 newPos = new Vector3(-1600, 0, 0);
+    private Vector3 speed = Vector3.zero;
+    [SerializeField]
+    private float smoothTime = 0.5f;
+
+    [SerializeField] private GameObject[] buttons;
+    private int pageNum;
+    private bool go;
+    void Start()
     {
-        //pages = GetComponentsInChildren<Transform>();
+        thisRect = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        if (transition)
+        if (go)
         {
-            if (left)
+            if (Vector3.Distance(thisRect.localPosition, newPos) > 1f)
             {
-                for (int i = 0; i < pages.Length; i++)
-                {
-                    if (pages[i].transform.position.x > origX[i] - 1600)
-                    {
-                        pages[i].transform.position += Vector3.left*speed*Time.deltaTime;
-                    }
-                    else
-                    {
-                        transition = false;
-                    }
-                }
+                thisRect.localPosition = Vector3.SmoothDamp(thisRect.localPosition, newPos, ref speed, smoothTime);
             }
             else
             {
-                for (int i = 0; i < pages.Length; i++)
-                {
-                    if (pages[i].transform.position.x < origX[i] + 1600)
-                    {
-                        pages[i].transform.position += Vector3.right*speed*Time.deltaTime;
-                    }
-                    else
-                    {
-                        transition = false;
-                    }
-                }
+          
+                    buttons[1].SetActive(!(pageNum >= 4));
+                buttons[0].SetActive(!(pageNum <= 0));
+
+                go = false;
             }
         }
-        else
+    }
+
+    public void Left()
+    {
+        if (!go)
         {
-            for (int i = 0; i < pages.Length; i++)
-            {
-                origX[i] = pages[i].transform.position.x;
-            }
+
+            newPos = new Vector3(thisRect.localPosition.x + 1600, 0, 0);
+           // Debug.Log(newPos);
+            pageNum--;
+            go = true;
+        }
+    }
+
+    public void Right()
+    {
+        if (!go)
+        {
+            pageNum++;
+            newPos = new Vector3(thisRect.localPosition.x - 1600, 0, 0);
+            go = true;
         }
     }
 }
